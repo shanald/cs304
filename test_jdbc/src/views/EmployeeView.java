310bd571e0bea255794ec6df5a89688a302a29c1
AlexStuckless <stuckless.a@gmail.com>
2016-03-23 19:37:30 -0700

    ready to implement listeners

--- test_jdbc/src/views/EmployeeView.java
+++ test_jdbc/src/views/EmployeeView.java
@@ -8,6 +8,9 @@ import javax.swing.*;
 
 public class EmployeeView extends JPanel{
 	
+	private JList list;
+	private DefaultListModel model;
+	
 	public EmployeeView(){
 		initPanel();
 		this.setVisible(true);
@@ -20,6 +23,8 @@ public class EmployeeView extends JPanel{
 	 */
 	public void initPanel() {
 		setUpCheckBoxes();
+		setUpFilters();
+		setUpResultsSpace();
     }
 	
 	private void setUpCheckBoxes(){
@@ -30,7 +35,6 @@ public class EmployeeView extends JPanel{
 		JLabel projectText = new JLabel("Select possible projections for employees:");
 		projectText.setForeground(Color.DARK_GRAY);
 		projectText.setBackground(Color.GRAY);
-		projectText.setSize(1000, 200);
 		projectText.setFont(new Font("Serif", Font.PLAIN, 24));
 		this.add(projectText);
 		for(int i = 0; i < numAttr; i++){
@@ -39,6 +43,45 @@ public class EmployeeView extends JPanel{
 			checkArray[i].setFont(new Font("Serif", Font.PLAIN, 24));
 			checkArray[i].setSelected(true);
 		}
+	}
+	
+	private void setUpFilters(){
+		JLabel projectText = new JLabel("Filter employees on:");
+		projectText.setForeground(Color.DARK_GRAY);
+		projectText.setBackground(Color.GRAY);
+		projectText.setFont(new Font("Serif", Font.PLAIN, 24));
+		this.add(projectText);
 		
+		//is an instructor
+		JCheckBox isInstructor = new JCheckBox("is an instructor");
+		this.add(isInstructor);
+		isInstructor.setFont(new Font("Serif", Font.PLAIN, 24));
+		isInstructor.setSelected(false);
+		
+		//is a manager
+		JCheckBox isManager = new JCheckBox("is a manager");
+		this.add(isManager);
+		isManager.setFont(new Font("Serif", Font.PLAIN, 24));
+		isManager.setSelected(false);
+		
+		//name starts with
+		JTextField nameField = new JTextField(24);
+		this.add(nameField);
+		
+		JLabel nameLabel = new JLabel("name starts with");
+		nameLabel.setForeground(Color.DARK_GRAY);
+		nameLabel.setBackground(Color.GRAY);
+		nameLabel.setFont(new Font("Serif", Font.PLAIN, 24));
+		this.add(nameLabel);
+	}
+	
+	private void setUpResultsSpace(){
+		 model = new DefaultListModel<String>();
+	     list = new JList<String>(model);
+	     list.setFixedCellWidth(1000);
+	     for (int i = 0; i < 15; i++){
+	    	 model.addElement("Element " + i);
+	     }
+	    this.add(list);
 	}
 }
